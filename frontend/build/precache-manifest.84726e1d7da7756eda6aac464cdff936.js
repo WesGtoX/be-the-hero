562f79b8a4b133ad1b1711f0c6d027f7c7bc1fbf
Wesley Mendes <wesley_gto_x@yahoo.com.br>
2020-04-01 03:44:32 -0300

    Build the Web App to deploy

---
+++ frontend/build/precache-manifest.84726e1d7da7756eda6aac464cdff936.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13678ad3cb74408df8c99ce3c34b6c99",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0efd427c36e9e09874cd",
+    "url": "/static/css/main.edfee68c.chunk.css"
+  },
+  {
+    "revision": "e4a871cd84d699c193b0",
+    "url": "/static/js/2.d1a32e62.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.d1a32e62.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0efd427c36e9e09874cd",
+    "url": "/static/js/main.a9ea556a.chunk.js"
+  },
+  {
+    "revision": "b05506d192fa13a4b14c",
+    "url": "/static/js/runtime-main.cb08f1e8.js"
+  },
+  {
+    "revision": "1f4b5508873e87c04ab679e647a7954b",
+    "url": "/static/media/heroes.1f4b5508.png"
+  },
+  {
+    "revision": "7eea718b48893e461f82049ec46c1e1c",
+    "url": "/static/media/logo.7eea718b.svg"
+  }
+]);
